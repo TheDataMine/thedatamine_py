fd9f531f825c5b0318444966eda81cb8b9e83d25
kevinamstutz <kevin@amstutz.io>
2020-11-17 14:41:02 -0600

    Update package

--- thedatamine/core.py
+++ thedatamine/core.py
@@ -3,16 +3,11 @@
 This module contains a set of functions that are currently used as complimentary material for Purdue University's integrative data science initiative, The Data Mine. 
 """
 
-def hello_datamine(name: str = 'student') -> str:
-    """Prints and returns a hello message to a Data Mine student.
+def hello_datamine(name: str = 'student') -> None:
+    """Prints a hello message to a Data Mine student.
 
     Args:
         str (name, optional): The name of a student. Defaults to 'student'.
-
-    Returns:
-        str: A potentially customized hello message.
     """
     msg = f'Hello {name}! Welcome to The Data Mine!'
     print(msg)
-
-    return msg
